862df4dee5fe79e15d5e491180c1aa279ad6aa63
SSollestre <sean.sollestre@gmail.com>
2025-02-23 19:21:50 -0800

    Set up file structure and router

---
+++ frontend/src/Homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function Homepage() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Hello World</p>
+    </>
+  );
+}
+
+export default Homepage;
